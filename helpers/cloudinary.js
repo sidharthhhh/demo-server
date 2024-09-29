373613e2e349e5be8ceb95b269757172a6c6f16f
sidharthhhh <swayam1044@gmail.com>
2024-09-29 21:19:04 +0530

    first commit

---
+++ helpers/cloudinary.js
@@ -0,0 +1,26 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+// Load environment variables
+require('dotenv').config();
+
+// Update Cloudinary configuration to use environment variables
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, imageUploadUtil };
